06ac9f923046225033aa5ce1c0c1b994ec15a37c
Kotikov <11deadsluts@gmail.com>
2022-10-12 23:57:27 +0300

    checking dog in C#

---
+++ dogfriends/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+
+while (distance > 10)
+{
+    if (friend = 1) 
+        {
+            double time = distance/(firstFriendSpeed+dogSpeed);
+        int friend = 2;
+     }
+     else
+        {
+            double time = distance/(secondFriendSpeed+dogSpeed);
+     }
+distance = distance - (firstFriendSpeed + secondFriendSpeed)*time;
+int count = count + 1;
+
+}
+
+Console.Write ("собака пробежала:");
+Console.Write (count);
+Console.Write ("раз:");
